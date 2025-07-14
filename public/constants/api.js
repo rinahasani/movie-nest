80c6f323cf57576a962615aa20af6749fb5dfd25
florinda <hasani.florinda@gmail.com>
2025-07-14 22:53:21 +0200

    Revert "still not done, a blueprint to have pushed"
    This reverts commit 066bc3e0ab7f64a3b4536d4bccaed988bb37dbf6.
    pushed to the wrong branch

--- public/constants/api.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = ""; //your api key here
-export const API_ENDPOINTS = {
-  DISCOVER: `${BASE_URL}/discover/movie?api_key=${API_KEY}`,
-  GENRES: `${BASE_URL}/genre/movie/list`,
-  //   MOVIE_DETAILS: `${BASE_URL}/movie/${movie_id}`,
-};
