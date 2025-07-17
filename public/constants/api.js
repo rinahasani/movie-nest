356b24fd97098e7dddfb6e2122223ad4ea44ff3d
rinahasani <rina.hasani@live.com>
2025-07-17 13:16:46 +0200

    fix: forgot to remove api key :(

--- public/constants/api.js
+++ public/constants/api.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = "4dbdd164e78dcc5d587179c7a0851ff2"; //your api key here
+const API_KEY = ""; //your api key here
 export const API_ENDPOINTS = {
   DISCOVER: `${BASE_URL}/discover/movie?api_key=${API_KEY}`,
   GENRES: `${BASE_URL}/genre/movie/list`,
