7a0e952cd1ed2cb932b52a40c44fb2498987ba8a
albanaR <albanarexhepi6@gmail.com>
2025-07-21 10:14:20 +0200

    remove apikey

--- constants/api.js
+++ constants/api.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = "fdb91272c3f95e9e093b31f73184bd5c"; //your api key here
+const API_KEY = ""; //your api key here
 export const API_ENDPOINTS = {
   DISCOVER:(page = 1,language = 'en-EN') => `${BASE_URL}/discover/movie?page=${page}&api_key=${API_KEY}&language=${language}`,
   // GENRES: (language = 'en-EN') `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=${language}`,
